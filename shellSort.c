92f10170f7f3b61af1b29ede1afe08cc42f4247c
msAranha <matheussphb@gmail.com>
2015-12-01 18:38:23 -0300

    Update shellSort.c

--- shellSort.c
+++ shellSort.c
@@ -3,35 +3,47 @@
 #include <time.h>
 
 struct vetor1{
-   int vet[1000];
-   int cVet[1000];
-   int dVet[1000];
+ int vet[1000];
+ int cVet[1000];
+ int dVet[1000];
 }v1;
 
 struct vetor2{
-    int vet[10000];
-    int cVet[10000];
-    int dVet[10000];
+    int vet[5000];
+    int cVet[5000];
+    int dVet[5000];
 }v2;
 
 struct vetor3{
-    int vet[100000];
-    int cVet[100000];
-    int dVet[100000];
+    int vet[25000];
+    int cVet[25000];
+    int dVet[25000];
 }v3;
 
 struct vetor4{
+    int vet[125000];
+    int cVet[125000];
+    int dVet[125000];
+}v4;
+
+struct vetor5{
+    int vet[625000];
+    int cVet[625000];
+    int dVet[625000];
+}v5;
+
+struct vetor6{
     int vet[1000000];
     int cVet[1000000];
     int dVet[1000000];
-}v4;
+}v6;
 
-int
+static int
 cres(const int *a, const int *b){
     return *a - *b;
 }
 
-int
+static int
 decres(const int *a, const int *b){
     return *b - *a;
 }
@@ -48,103 +60,160 @@ functVetor(int *vetor, int *cVetor, int *dVetor, int tam){
 }
 
 void
-shellSort(int *vetor, int tam){
+shellSort(int *vetor, int tam, unsigned long int *comp, unsigned long int *atrib){
     int i, j, intervalo, aux, cont;
-    unsigned long int inter = 0, associ = 0;
+
     intervalo = (tam-1)/2;
 
     for(cont = intervalo; cont > 0; cont--){
         for(i = 0, j = intervalo; j < tam; i++, j++){
-            inter++;
+            (*comp)++;
             if(vetor[i] > vetor[j]){
                 aux = vetor[i];
                 vetor[i] = vetor[j];
                 vetor[j] = aux;
-                associ++;
+                (*atrib)++;
             }
         }
     }
-    printf("Comparações: %lu\nAtribuições: %lu\n", inter, associ);
 }
 
 void
-inciShellSort(){
-    float t1 = 0.0, t2 = 0.0;
+shellSort_noOrder(){
+    double t1 = 0.0, t2 = 0.0;
+    unsigned long int comp, atrib;
 
-    printf("ShellSort - 1.000 posições [DESORDENADO]: \n");
+    printf("\nSEM ORDENAÇÃO\n");
+    printf("Tamanho\tTempo\tComparação\tAtribuição\n");
+
+    comp = 0, atrib = 0;
     t1 = clock();
-    shellSort(v1.vet, sizeof(v1.vet) / sizeof(int));
+    shellSort(v1.vet, sizeof(v1.vet) / sizeof(int), &comp, &atrib);
     t2 = clock();
-    printf("Tempo: %fms\n", (t2 - t1) / CLOCKS_PER_SEC);
+    printf("%lu\t%f\t%lu\t%lu\n", sizeof(v1.vet) / sizeof(int), (t2 - t1) / CLOCKS_PER_SEC, comp, atrib);
 
-    printf("ShellSort - 1.000 posições [ORDENADO CRESCENTE]: \n");
+    comp = 0, atrib = 0;
     t1 = clock();
-    shellSort(v1.cVet, sizeof(v1.vet) / sizeof(int));
+    shellSort(v2.vet, sizeof(v2.vet) / sizeof(int), &comp, &atrib);
     t2 = clock();
-    printf("Tempo: %fms\n", (t2 - t1) / CLOCKS_PER_SEC);
+    printf("%lu\t%f\t%lu\t%lu\n", sizeof(v2.vet) / sizeof(int), (t2 - t1) / CLOCKS_PER_SEC, comp, atrib);
 
-    printf("ShellSort - 1.000 posições - [ORDENADO DECRESCENTE]: \n");
+    comp = 0, atrib = 0;
     t1 = clock();
-    shellSort(v1.dVet, sizeof(v1.vet) / sizeof(int));
+    shellSort(v3.vet, sizeof(v3.vet) / sizeof(int), &comp, &atrib);
     t2 = clock();
-    printf("Tempo: %fms\n", (t2 - t1) / CLOCKS_PER_SEC);
+    printf("%lu\t%f\t%lu\t%lu\n", sizeof(v3.vet) / sizeof(int), (t2 - t1) / CLOCKS_PER_SEC, comp, atrib);
 
-
-    printf("\n\nShellSort - 10.000 posições [DESORDENADO]: \n");
+    comp = 0, atrib = 0;
     t1 = clock();
-    shellSort(v2.vet, sizeof(v2.vet) / sizeof(int));
+    shellSort(v4.vet, sizeof(v4.vet) / sizeof(int), &comp, &atrib);
     t2 = clock();
-    printf("Tempo: %fms\n", (t2 - t1) / CLOCKS_PER_SEC);
+    printf("%lu\t%f\t%lu\t%lu\n", sizeof(v4.vet) / sizeof(int), (t2 - t1) / CLOCKS_PER_SEC, comp, atrib);
 
-    printf("ShellSort - 10.000 posições [ORDENADO CRESCENTE]: \n");
+    comp = 0, atrib = 0;
     t1 = clock();
-    shellSort(v2.cVet, sizeof(v2.vet) / sizeof(int));
+    shellSort(v5.vet, sizeof(v5.vet) / sizeof(int), &comp, &atrib);
     t2 = clock();
-    printf("Tempo: %fms\n", (t2 - t1) / CLOCKS_PER_SEC);
+    printf("%lu\t%f\t%lu\t%lu\n", sizeof(v5.vet) / sizeof(int), (t2 - t1) / CLOCKS_PER_SEC, comp, atrib);
 
-    printf("ShellSort - 10.000 posições - [ORDENADO DECRESCENTE]: \n");
+    comp = 0, atrib = 0;
     t1 = clock();
-    shellSort(v2.dVet, sizeof(v2.vet) / sizeof(int));
+    shellSort(v6.vet, sizeof(v6.vet) / sizeof(int), &comp, &atrib);
     t2 = clock();
-    printf("Tempo: %fms\n", (t2 - t1) / CLOCKS_PER_SEC);
+    printf("%lu\t%f\t%lu\t%lu\n", sizeof(v6.vet) / sizeof(int), (t2 - t1) / CLOCKS_PER_SEC, comp, atrib);
 
+}
 
-    printf("\n\nShellSort - 100.000 posições [DESORDENADO]: \n");
+void
+shellSort_cresOrder(){
+    double t1 = 0.0, t2 = 0.0;
+    unsigned long int comp, atrib;
+
+    printf("\n\nORDENADO CRESCENTE\n");
+    printf("Tamanho\tTempo\tComparação\tAtribuição\n");
+
+    comp = 0, atrib = 0;
     t1 = clock();
-    shellSort(v3.vet, sizeof(v3.vet) / sizeof(int));
+    shellSort(v1.cVet, sizeof(v1.cVet) / sizeof(int), &comp, &atrib);
     t2 = clock();
-    printf("Tempo: %fms\n", (t2 - t1) / CLOCKS_PER_SEC);
+    printf("%lu\t%f\t%lu\t%lu\n", sizeof(v1.vet) / sizeof(int), (t2 - t1) / CLOCKS_PER_SEC, comp, atrib);
 
-    printf("ShellSort - 100.000 posições [ORDENADO CRESCENTE]: \n");
+    comp = 0, atrib = 0;
     t1 = clock();
-    shellSort(v3.cVet, sizeof(v3.vet) / sizeof(int));
+    shellSort(v2.cVet, sizeof(v2.cVet) / sizeof(int), &comp, &atrib);
     t2 = clock();
-    printf("Tempo: %fms\n", (t2 - t1) / CLOCKS_PER_SEC);
+    printf("%lu\t%f\t%lu\t%lu\n", sizeof(v2.vet) / sizeof(int), (t2 - t1) / CLOCKS_PER_SEC, comp, atrib);
 
-    printf("ShellSort - 100.000 posições - [ORDENADO DECRESCENTE]: \n");
+    comp = 0, atrib = 0;
     t1 = clock();
-    shellSort(v3.dVet, sizeof(v3.vet) / sizeof(int));
+    shellSort(v3.cVet, sizeof(v3.vet) / sizeof(int), &comp, &atrib);
     t2 = clock();
-    printf("Tempo: %fms\n", (t2 - t1) / CLOCKS_PER_SEC);
+    printf("%lu\t%f\t%lu\t%lu\n", sizeof(v3.vet) / sizeof(int), (t2 - t1) / CLOCKS_PER_SEC, comp, atrib);
 
-
-    printf("\n\nShellSort - 1.000.000 posições [DESORDENADO]: \n");
+    comp = 0, atrib = 0;
     t1 = clock();
-    shellSort(v4.vet, sizeof(v4.vet) / sizeof(int));
+    shellSort(v4.cVet, sizeof(v4.vet) / sizeof(int), &comp, &atrib);
     t2 = clock();
-    printf("Tempo: %fms\n", (t2 - t1) / CLOCKS_PER_SEC);
+    printf("%lu\t%f\t%lu\t%lu\n", sizeof(v4.vet) / sizeof(int), (t2 - t1) / CLOCKS_PER_SEC, comp, atrib);
 
-    printf("ShellSort - 1.000.000 posições [ORDENADO CRESCENTE]: \n");
+    comp = 0, atrib = 0;
     t1 = clock();
-    shellSort(v4.cVet, sizeof(v4.vet) / sizeof(int));
+    shellSort(v5.cVet, sizeof(v5.vet) / sizeof(int), &comp, &atrib);
     t2 = clock();
-    printf("Tempo: %fms\n", (t2 - t1) / CLOCKS_PER_SEC);
+    printf("%lu\t%f\t%lu\t%lu\n", sizeof(v5.vet) / sizeof(int), (t2 - t1) / CLOCKS_PER_SEC, comp, atrib);
 
-    printf("ShellSort - 1.000.000 posições - [ORDENADO DECRESCENTE]: \n");
+    comp = 0, atrib = 0;
     t1 = clock();
-    shellSort(v4.dVet, sizeof(v4.vet) / sizeof(int));
+    shellSort(v6.cVet, sizeof(v6.vet) / sizeof(int), &comp, &atrib);
     t2 = clock();
-    printf("Tempo: %fms\n", (t2 - t1) / CLOCKS_PER_SEC);
+    printf("%lu\t%f\t%lu\t%lu\n", sizeof(v6.vet) / sizeof(int), (t2 - t1) / CLOCKS_PER_SEC, comp, atrib);
+
+}
+
+void
+shellSort_decresOrder(){
+    double t1 = 0.0, t2 = 0.0;
+    unsigned long int comp, atrib;
+
+    printf("\n\nORDENADO DECRESCENTE\n");
+    printf("Tamanho\tTempo\tComparação\tAtribuição\n");
+
+    comp = 0, atrib = 0;
+    t1 = clock();
+    shellSort(v1.dVet, sizeof(v1.vet) / sizeof(int), &comp, &atrib);
+    t2 = clock();
+    printf("%lu\t%f\t%lu\t%lu\n", sizeof(v1.vet) / sizeof(int), (t2 - t1) / CLOCKS_PER_SEC, comp, atrib);
+
+    comp = 0, atrib = 0;
+    t1 = clock();
+    shellSort(v2.dVet, sizeof(v2.cVet) / sizeof(int), &comp, &atrib);
+    t2 = clock();
+    printf("%lu\t%f\t%lu\t%lu\n", sizeof(v2.vet) / sizeof(int), (t2 - t1) / CLOCKS_PER_SEC, comp, atrib);
+
+    comp = 0, atrib = 0;
+    t1 = clock();
+    shellSort(v3.dVet, sizeof(v3.vet) / sizeof(int), &comp, &atrib);
+    t2 = clock();
+    printf("%lu\t%f\t%lu\t%lu\n", sizeof(v3.vet) / sizeof(int), (t2 - t1) / CLOCKS_PER_SEC, comp, atrib);
+
+    comp = 0, atrib = 0;
+    t1 = clock();
+    shellSort(v4.dVet, sizeof(v4.vet) / sizeof(int), &comp, &atrib);
+    t2 = clock();
+    printf("%lu\t%f\t%lu\t%lu\n", sizeof(v4.vet) / sizeof(int), (t2 - t1) / CLOCKS_PER_SEC, comp, atrib);
+
+    comp = 0, atrib = 0;
+    t1 = clock();
+    shellSort(v5.dVet, sizeof(v5.vet) / sizeof(int), &comp, &atrib);
+    t2 = clock();
+    printf("%lu\t%f\t%lu\t%lu\n", sizeof(v5.vet) / sizeof(int), (t2 - t1) / CLOCKS_PER_SEC, comp, atrib);
+
+    comp = 0, atrib = 0;
+    t1 = clock();
+    shellSort(v6.dVet, sizeof(v6.vet) / sizeof(int), &comp, &atrib);
+    t2 = clock();
+    printf("%lu\t%f\t%lu\t%lu\n", sizeof(v6.vet) / sizeof(int), (t2 - t1) / CLOCKS_PER_SEC, comp, atrib);
+
 }
 
 int
@@ -154,8 +223,14 @@ main(){
     functVetor(v2.vet, v2.cVet, v2.dVet, sizeof(v2.vet) / sizeof(int));
     functVetor(v3.vet, v3.cVet, v3.dVet, sizeof(v3.vet) / sizeof(int));
     functVetor(v4.vet, v4.cVet, v4.dVet, sizeof(v4.vet) / sizeof(int));
+    functVetor(v5.vet, v5.cVet, v5.dVet, sizeof(v5.vet) / sizeof(int));
+    functVetor(v6.vet, v6.cVet, v6.dVet, sizeof(v6.vet) / sizeof(int));
 
-    inciShellSort();
+
+    printf("- - - - - - - - - - METODO shellSort - - - - - - - - - -");
+    shellSort_noOrder();
+    shellSort_cresOrder();
+    shellSort_decresOrder();
 
     return 0;
 }
